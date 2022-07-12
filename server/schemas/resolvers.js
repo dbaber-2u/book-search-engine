3a7941c2d48b9da41ed57420e9e878ab91be1e1c
Baber <dnbaber@hotmail.com>
2022-07-11 23:22:23 -0400

    add code behind mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,6 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
-const { User } = required('../models');
+const { User, Book } = required('../models');
 
 const resolvers = {
     Query: {
@@ -17,17 +17,58 @@ const resolvers = {
         }
     },
     Mutation: {
-        addUser: async () => {
-
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+      
+            return { token, user };
         },
-        login: async () => {
-
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const token = signToken(user);
+            return { token, user };
         },
-        saveBook: async () => {
-
+        saveBook: async (parent, args, context) => {
+            if (context.user) {
+              const book = await Book.create({ ...args});
+          
+              const user = await User.findByIdAndUpdate(
+                { _id: context.user._id },
+                { $push: { savedBooks: book } },
+                { new: true }
+              );
+          
+              return user;
+            }
+          
+            throw new AuthenticationError('You need to be logged in!');
         },
-        removeBook: async () => {
+        removeBook: async (parent, args, context) => {
+            if (context.user) {
+                const book = await Book.findOne({ bookId: args.bookId });
+            
+                const user = await User.findByIdAndUpdate(
+                  { _id: context.user._id },
+                  { $delete: { savedBooks: book } },
+                  { new: true }
+                );
+            
 
+                return user;
+              }
+            
+              throw new AuthenticationError('You need to be logged in!');
         }
     }
 };
