2bc59f7a2a4c86bbb8c61d41f4a85f78137a7866
Baber <dnbaber@hotmail.com>
2022-07-12 22:40:45 -0400

    update deleteBook functionality.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -46,9 +46,17 @@ export const SAVE_BOOK = gql`
 export const REMOVE_BOOK = gql`
   mutation removeBook($bookId: ID!) {
     removeBook(bookId: $bookId) {
-      user {
-        _id
-        username
+      _id
+      username
+      email
+      bookCount
+      savedBooks { 
+        bookId
+        authors
+        description
+        title
+        image
+        link
       }
     }
   }
